1b40144971741563e636c2e6f68553855864248b
DanielKhazer <115623150+DanielKhazer@users.noreply.github.com>
2023-09-05 17:49:18 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Double> list = new ArrayList<>();
+        System.out.println("EOOOOOW dig");
+        Scanner tal = new Scanner(System.in);
+
+        System.out.println("Indtast tal");
+
+        String input = tal.next();
+
+        list.add(Double.parseDouble(input));
+        while (true){
+
+
+        System.out.println("Indtast endnu et tal");
+
+        input = tal.next();
+
+        list.add(Double.parseDouble(input));
+
+        Double udregnet = calculateSum(list);
+        System.out.println("summen blev udregnet til " + udregnet);
+
+        Double gennemsnit = udregnet / list.size();
+        System.out.println("Gennemsnittet blev udregnet til " + gennemsnit);
+        }
+    }
+    public static Double calculateSum(ArrayList<Double> list){
+        Double sum = 0.0;
+        for (int i = 0; i < list.size(); i++) {
+        //kode her
+            sum += list.get(i);
+        }
+        return sum;
+    }
+}
